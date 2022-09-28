92c6dec86603f0b8362809b54a3b2570cfa8aec3
Jordi <jordi.fontan@gmail.com>
2022-09-28 17:21:38 +0200

    Add findGeneThreeCondonStop  code + test

--- c2-Genetics/src/dukejavac2w2/FindGeneWhile.java
+++ c2-Genetics/src/dukejavac2w2/FindGeneWhile.java
@@ -1,15 +1,40 @@
 package dukejavac2w2;
 
+import javax.swing.plaf.synth.SynthTreeUI;
+
 public class FindGeneWhile {
 
 	public FindGeneWhile() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static int findStopCodon(String str, int startIndex,String codon){
+	public static int findStopCodon(String str,
+			int startIndex,String stopCodon){
 		// There are actually three stop codons TAA + 2 more
+		// find stopCodon starting from (startIndex +3) save as currIndex
 		
-		return 0;
+		int currIndex = str.indexOf(stopCodon,startIndex+3);
+			
+		
+		// as long as currIndex is not qual to -1
+		while (currIndex != -1)
+		{			// check if currIndex-startIndex is 3 mult
+			
+			// if so, return currIndex (we are searching an index now)
+			if ((currIndex-startIndex)%3 == 0) {
+				return currIndex;
+			}
+			else
+			{	
+			// if not, update currIndex and look again starting from currInd + 1
+			currIndex= str.indexOf(stopCodon, currIndex +1);
+			}
+		}	
+		
+		// if we exit loop we didn't find any Index so return str.lenght()
+		// (to make the minimum strategy work)
+		
+		return str.length();
 		
 		
 		
@@ -91,7 +116,24 @@ public static String findGeneThreeCondonStop(String dna) {
 			System.out.println(findGeneOneCondonStop(dna2));
 			String dna3= "ATGGTTGATAAGCCTAAGCTATAA";
 			System.out.println(findGeneOneCondonStop(dna3));
-	// "CCATGCGCTTAATGATAGATTAA" for 3stops testing
+			testFindStopCodon();
 	}
-
+	public static void testFindStopCodon() {
+		// "CCATGCGCTTAATGATAGATTAA" for 3stops testing
+		
+	//            012345678901234567890123456
+	String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxxx";
+	
+		int dex = findStopCodon(dna, 0, "TAA");
+	         if(dex !=9 ) System.out.println("error on 9") ;	
+	         dex = findStopCodon(dna, 9, "TAA");
+	         if(dex !=21 ) System.out.println("error on 21");	
+	         dex = findStopCodon(dna, 1, "TAA");
+	         if(dex !=26 ) System.out.println("error on 26 TAA");
+	         dex = findStopCodon(dna, 0, "TAG");
+	         if(dex !=26 ) System.out.println("error on 26 TAG");
+	         dex = findStopCodon(dna, 0, "TGA");
+	         if(dex !=26 ) System.out.println("error on 26 TGA");
+	         
+		}
 }
