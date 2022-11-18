0b2cf0bfdc4a5afd4a5ae431944a8139de1697d7
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2022-11-18 17:45:15 +0100

    upgrade junit version

--- c3-Structured Data/m2-randomStory/randomStory/src/test/java/com/dalneg/portfolio/storyTelling/TestWordCount.java
+++ c3-Structured Data/m2-randomStory/randomStory/src/test/java/com/dalneg/portfolio/storyTelling/TestWordCount.java
@@ -1,39 +1,47 @@
 package com.dalneg.portfolio.storyTelling;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+@DisplayName(" Testing Word COunt Class")
 public class TestWordCount {
 static WordCount wc;
-	@BeforeClass
+	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
-		
-		
+		 wc=new WordCount("c:\\sharedData\\testTextFileToString.txt");
+		System.out.println("Before Class");
 	}
 
-	@AfterClass
+	@AfterAll
 	public static void tearDownAfterClass() throws Exception {
+	
+		System.out.println("After Class");
 	}
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
+		System.out.println("Before ");
+		
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
+		System.out.println("After");
 	}
 
 	@Test
 	public final void testTextFiletoString() {
-		WordCount wc=new WordCount("c:\\sharedData\\testTextFileToString.txt");
+		//WordCount wc=new WordCount("c:\\sharedData\\testTextFileToString.txt");
 		String test=null;
 		try {
 			test=wc.textFileToString("c:\\sharedData\\testTextFileToString.txt");
@@ -46,9 +54,10 @@ static WordCount wc;
 		assertTrue("This is a test".equals(test.strip()));
 	}
 
-	@Test
+	@org.junit.jupiter.api.Test
+	@DisplayName("A list with the words of a string")
 	public final void testFromStringToList() {
-		WordCount wc=new WordCount("c:\\sharedData\\testTextFileToString.txt");
+		//WordCount wc=new WordCount("c:\\sharedData\\testTextFileToString.txt");
 		List<String> list=new ArrayList<>();
 		list.add("This");
 		list.add("is");
@@ -56,8 +65,9 @@ static WordCount wc;
 		list.add("test");
 		
 		String s="This is a test";
-		
-		assertTrue(list.equals(wc.fromStringToList(s)));
+		@SuppressWarnings("unused")
+		String message="The list with a string 'This is a test' should be equels to test method with 'This is a test' as parameter \t\n";
+		assertEquals(list,wc.fromStringToList(s));
 	}
 	
 }
